41838c18436929966ec59d94cd17bf4356f70eec
Jason Guo <jsonvi@gmail.com>
2009-02-02 08:28:59 +0000

    add the feature that implement the Issue 6

--- chrome/content/preloader.js
+++ chrome/content/preloader.js
@@ -2,20 +2,79 @@
 var Preloader = function(){
   //private
   //
-  var pageExist = function(_path){
-    var fileIn = FileIO.open(_path);
+  var Page = function(){
+    return{
+      name:null,
+      exist:false,
+      file:null,
+      withPath:function(){
+        var name = this.name;
+        var index = name.indexOf("\/");
+        return (index != -1);
+      }
+    };
+  };
+  var allpaths = new Array();
+  allpaths[0] = Options.getPath();
+  var pageExist = function(_file){
+    var fileIn = FileIO.open(_file);
     return fileIn.exists();
   };
+  var setNonExist = function(_link){
+    _link.setAttribute('class','non-exist-inner-link'); 
+  };
+  var initPath = function(dir){
+     var rootlist = new Array();
+     if (dir.exists())
+      rootlist = DirIO.getSubFolders(dir); 
+     if (rootlist.length > 0) {
+        for (var i = 0; i < rootlist.length; i++) {
+          allpaths.push(rootlist[i].path);
+          initPath(rootlist[i]);
+        }
+      }
+     return rootlist;
+  };
+  var searchInHomePath = function(_page){
+    for(var i=0;i<allpaths.length;i++)
+    {
+       var _file = allpaths[i]+'/'+_page;
+       var fileIn = FileIO.open(_file);
+       if(fileIn.exists())
+         return _file;
+    }
+    return false;
+  };
+
   //public
   return {
     initLink: function(){
+     initPath(DirIO.open(Options.getPath()));
      var arr = doc.getElementsByClassName('InnerLink'); 
      for(var i=0;i<arr.length;)
      {
-        if(!pageExist(arr.item(i).getAttribute('href')))
-          arr[i].setAttribute('class','NonExistInnerLink');
-        else
-          i++;
+       var page = new Page();
+       page.name = arr.item(i).getAttribute('href');
+       page.file = Options.getPath()+'/'+page.name; 
+       var withpath = page.withPath();
+       if(!withpath)
+       {
+         var realpath = searchInHomePath(page.name);
+         if(realpath)
+         {
+           page.file = realpath;
+           page.exist = true;
+           arr.item(i).setAttribute('href',realpath);
+         }
+       }
+       else
+         page.exist = pageExist(page.file);
+
+       if(page.exist)
+         i++;
+       else
+         arr.item(i).setAttribute('class','non-exist-inner-link'); 
+
      }
     }  
   };
